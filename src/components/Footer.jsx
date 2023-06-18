a110929ea3f9d76624d5cea9be7d1fbadbc0e1dc
omar-pherones <omarpherones@gmail.com>
2023-06-18 19:07:43 +0600

    now build added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom';
+import FooterLogo from '../img/logo.png';
 const Footer = () => {
     return (
-        <footer className="py-8 flex flex-col gap-3 items-center bg-rose-200 opacity-75">
-            <h2 className="text-2xl font-bold lowercase italic">
-                Food<span className="text-rose-500">verse</span>
-            </h2>
+        <footer className="py-8 flex flex-col gap-3 items-center bg-rose-200/75 opacity-75">
+            <Link
+                className="footerlogo text-2xl flex items-center gap-2 lowercase font-bold"
+                to="/"
+            >
+                <img
+                    src={FooterLogo}
+                    alt="food"
+                    className="w-12 overflow-hidden object-cover"
+                />
+                <span>Food</span>
+                <span className="text-rose-500">Villa</span>
+            </Link>
             <p>
                 &copy; {new Date().getFullYear()} Foodverse. All rights
                 reserved.
